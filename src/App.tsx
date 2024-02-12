a5f000f0d5ea86f3ef9d6e027ccd53016449c048
Kiley Mitti <kiley.mitti@rocketcom.com>
2024-02-12 16:49:04 -0500

    initial implementation (before refactor)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,155 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { RuxCard, RuxClock, RuxGlobalStatusBar, RuxIcon } from '@astrouxds/react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RuxGlobalStatusBar app-domain='ACME' app-name='PLATFORM' menu-icon='apps' app-version='v1' app-state='App State' username='User Name' app-state-color=''>
+        <RuxClock></RuxClock>
+        <div className='icons spacer' slot='right-side'></div>
+      </RuxGlobalStatusBar>
+      <main>
+        <section className='content'>
+          <article id='home-content'>
+            <RuxCard className='service-card'>
+              <header slot='header'>
+                <RuxIcon icon='antenna' size='80px' className=''></RuxIcon>
+                <h3>AB</h3>
+              </header>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nostrum rerum praesentium.</p>
+              </div>
+              <footer slot='footer'>
+                <a href='https://monitor-ttc.netlify.app' target='_blank' className='appLink'>
+                  Open AB
+                </a>
+              </footer>
+            </RuxCard>
+
+            <RuxCard className='service-card'>
+              <header slot='header'>
+                <RuxIcon icon='explore' size='80px' className=''></RuxIcon>
+                <h3>FD</h3>
+              </header>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nostrum rerum praesentium.</p>
+              </div>
+              <footer slot='footer'>
+                <a href='https://ttc-command-react.netlify.app/' target='_blank' className='appLink'>
+                  Open FD
+                </a>
+              </footer>
+            </RuxCard>
+
+            <RuxCard className='service-card'>
+              <header slot='header'>
+                <RuxIcon icon='antenna-transmit' size='80px' className=''></RuxIcon>
+                <h3>GC</h3>
+              </header>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nostrum rerum praesentium.</p>
+              </div>
+              <footer slot='footer'>
+                <a href='https://grm-dashboard-react.netlify.app/' target='_blank' className='appLink'>
+                  Open GC
+                </a>
+              </footer>
+            </RuxCard>
+
+            <RuxCard className='service-card'>
+              <header slot='header'>
+                <RuxIcon icon='assignment' size='80px' className=''></RuxIcon>
+                <h3>MM</h3>
+              </header>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nostrum rerum praesentium.</p>
+              </div>
+              <footer slot='footer'>
+                <a href='https://grm-equipment-react-ts.netlify.app/' target='_blank' className='appLink'>
+                  Open MM
+                </a>
+              </footer>
+            </RuxCard>
+
+            <RuxCard className='service-card'>
+              <header slot='header'>
+                <RuxIcon icon='equipment' size='80px' className=''></RuxIcon>
+                <h3>MS</h3>
+              </header>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+              <footer slot='footer'>
+                <a href='https://grm-schedule-react.netlify.app/' target='_blank' className='appLink'>
+                  Open MS
+                </a>
+              </footer>
+            </RuxCard>
+
+            <RuxCard className='service-card'>
+              <header slot='header'>
+                <RuxIcon icon='pie-chart' size='80px' className=''></RuxIcon>
+                <h3>OD</h3>
+              </header>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nostrum rerum praesentium.</p>
+              </div>
+              <footer slot='footer'>
+                <a href='https://flight-dynamics-service.netlify.app' target='_blank' className='appLink'>
+                  Open OD
+                </a>
+              </footer>
+            </RuxCard>
+
+            <RuxCard className='service-card'>
+              <header slot='header'>
+                <RuxIcon icon='public' size='80px' className=''></RuxIcon>
+                <h3>OP</h3>
+              </header>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nostrum rerum praesentium.</p>
+              </div>
+              <footer slot='footer'>
+                <a href='https://flight-dynamics-service.netlify.app' target='_blank' className='appLink'>
+                  Open OP
+                </a>
+              </footer>
+            </RuxCard>
+
+            <RuxCard className='service-card'>
+              <header slot='header'>
+                <RuxIcon icon='satellite-transmit' size='80px' className=''></RuxIcon>
+                <h3>SCM</h3>
+              </header>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nostrum rerum praesentium.</p>
+              </div>
+              <footer slot='footer'>
+                <a href='https://astrouxds.com' target='_blank' className='appLink'>
+                  Open SCM
+                </a>
+              </footer>
+            </RuxCard>
+
+            <RuxCard className='service-card'>
+              <header slot='header'>
+                <RuxIcon icon='track-changes' size='80px' className=''></RuxIcon>
+                <h3>TC</h3>
+              </header>
+              <div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nostrum rerum praesentium.</p>
+              </div>
+              <footer slot='footer'>
+                <a href='https://flight-dynamics-service.netlify.app' target='_blank' className='appLink'>
+                  Open TC
+                </a>
+              </footer>
+            </RuxCard>
+          </article>
+        </section>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
